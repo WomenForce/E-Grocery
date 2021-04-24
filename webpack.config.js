f63e69dc17f932adc4e52ae3c1cec6bad5ba2af2
Isabelar07 <isabelarochas@outlook.com.br>
2021-04-24 20:52:49 -0300

    alterando imagem do banner das categorias

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,11 @@ var bootstrapPackages = {
 };
 
 module.exports = [{
-    mode: 'production',
+    mode: 'development',
     name: 'js',
     entry: jsFiles,
     output: {
-        path: path.resolve('./cartridges/app_storefront_base/cartridge/static'),
+        path: path.resolve('./cartridges/app_custom_siteEgrocery/cartridge/static'),
         filename: '[name].js'
     },
     module: {
@@ -49,7 +49,7 @@ module.exports = [{
     name: 'scss',
     entry: scssFiles,
     output: {
-        path: path.resolve('./cartridges/app_storefront_base/cartridge/static'),
+        path: path.resolve('./cartridges/app_custom_siteEgrocery/cartridge/static'),
         filename: '[name].css'
     },
     module: {
